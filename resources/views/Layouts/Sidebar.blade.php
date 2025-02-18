15d4e5389477f00dc2c69fc1c006631adf0c907e
MRizki28 <muhammadrizkitkj123@gmail.com>
2025-02-18 14:37:11 +0800

    add:fe

--- resources/views/Layouts/Sidebar.blade.php
+++ resources/views/Layouts/Sidebar.blade.php
@@ -9,8 +9,8 @@
 						<div class="info">
 							<a href="{{ url('/setting') }}" >
 								<span>
-									<span class="user-name" id="userName">{{ strip_tags(auth()->user()->name) }}</span>
-									<span class="user-level" id="user-level">{{ auth()->user()->role }}</span>
+									<span class="user-name" id="userName">Rizki</span>
+									<span class="user-level" id="user-level">Super admin</span>
 								</span>
 							</a>
 							<div class="clearfix"></div>
@@ -26,7 +26,6 @@
 						</div>
 					</div>
 					<ul class="nav nav-primary">
-						@if (auth()->user()->role == 'admin')
 						<li class="nav-item {{ request()->is('/*') ? 'active' : '' }}">
 							<a href="{{ url('/') }}">
 								<i class="fas fa-align-left"></i>
@@ -70,69 +69,6 @@
 								<p>Lowongan Pekerjaan</p>
 							</a>
 						</li>  
-						@else
-						<li class="nav-item {{ request()->is('/*') ? 'active' : '' }}">
-							<a href="{{ url('/') }}">
-								<i class="fas fa-align-left"></i>
-								<p>Beranda</p>
-							</a>
-						</li> 
-
-						<li class="nav-item {{ request()->is('penawaran*') ? 'active' : '' }}">
-							<a href="{{ url('/penawaran') }}">
-								<i class="fas fa-newspaper"></i>
-								<p>Penawaran</p>
-							</a>
-						</li>  
-						<li class="nav-item {{ request()->is('berita*') ? 'active' : '' }}">
-							<a href="{{ url('/berita') }}">
-								<i class="fas fa-newspaper"></i>
-								<p>Informasi & Berita</p>
-							</a>
-						</li>  
-						<li class="nav-item {{ request()->is('produk*') ? 'active' : '' }}">
-							<a href="{{ url('/produk') }}">
-								<i class="fab fa-product-hunt"></i>
-								<p>Produk</p>
-							</a>
-						</li>  
-						<li class="nav-item {{ request()->is('hero*') ? 'active' : '' }}">
-							<a href="{{ url('/hero') }}">
-								<i class="fas fa-file-image"></i>
-								<p>Banner</p>
-							</a>
-						</li>  
-						<li class="nav-item {{ request()->is('lelang*') ? 'active' : '' }}">
-							<a href="{{ url('/lelang') }}">
-								<i class="fas fa-home"></i>
-								<p>Lelang</p>
-							</a>
-						</li>  
-						<li class="nav-item {{ request()->is('manajemen-pengguna*') ? 'active' : '' }}">
-							<a href="{{ url('/manajemen-pengguna') }}">
-								<i class="fas fa-user-alt"></i>
-								<p>Manajemen Pengguna</p>
-							</a>
-						</li>  
-						<li class="nav-item {{ request()->is('karir*') ? 'active' : '' }}">
-							<a href="{{ url('/karir') }}">
-								<i class="fas fa-user-tie"></i>
-								<p>Lowongan Pekerjaan</p>
-							</a>
-						</li>  
-						<li class="nav-item {{ request()->is('backup*') ? 'active' : '' }}">
-							<a href="{{ url('/backup') }}">
-								<i class="fas fa-database"></i>
-								<p>Backup</p>
-							</a>
-						</li>  
-						<li class="nav-item {{ request()->is('report*') ? 'active' : '' }}">
-							<a href="{{ url('/report') }}">
-								<i class="fas fa-headset"></i>
-								<p>Report</p>
-							</a>
-						</li> 
-						@endif
 					<ul>
 				</div>
 			</div>
