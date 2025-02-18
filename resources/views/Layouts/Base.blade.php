abb33e7d9ebf39b5f4427034409ebe1382355be1
MRizki28 <muhammadrizkitkj123@gmail.com>
2025-02-18 14:37:07 +0800

    add:fe

--- resources/views/Layouts/Base.blade.php
+++ resources/views/Layouts/Base.blade.php
@@ -3,7 +3,7 @@
 
 <head>
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-    <title>BPR Palu Anugerah</title>
+    <title>STN BINA PALU</title>
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <meta name="key" content="{{ env('APP_KEY') }}">
     <meta content='width=device-width, initial-scale=1.0, shrink-to-fit=no' name='viewport' />
