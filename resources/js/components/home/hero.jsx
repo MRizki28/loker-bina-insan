059dc0c39e1bef15e57f4be8ae97c782d7e29e87
MRizki28 <muhammadrizkitkj123@gmail.com>
2025-02-18 14:36:48 +0800

    add:fe

---
+++ resources/js/components/home/hero.jsx
@@ -0,0 +1,21 @@
+export default function Hero() {
+    return (
+        <div className="bg-[url('static/img/hero.png')] h-[40rem] bg-cover bg-center">
+            <div className="bg-gradient-to-r from-cyan-500 to-blue-500 opacity-85 h-[40rem] grid content-center">
+                <div className="max-w-7xl mx-auto py-16 px-4 sm:py-24 sm:px-6 lg:px-8 ">
+                    <div className="text-center">
+                        <h2 className="text-base font-semibold text-white tracking-wide uppercase">
+                            Jadilah Pendidik Berkualitas, Membangun Masa Depan Bersama!
+                        </h2>
+                        <p className="mt-1 text-4xl font-extrabold text-white sm:text-5xl sm:tracking-tight lg:text-6xl">
+                            Temukan Peluang Karier Anda
+                        </p>
+                        <p className="max-w-xl mt-5 mx-auto text-xl text-white">
+                            Bergabunglah dengan STN Bina Insan Palu Untuk Membangun Masa Depan Bersama
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
