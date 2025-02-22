342276a4baef80336785e9a58d8c1837cb1fa6e3
MRizki28 <muhammadrizkitkj123@gmail.com>
2025-02-22 21:30:18 +0800

    update

---
+++ app/Interfaces/AuthInterfaces.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Interfaces;
+
+use App\Http\Requests\Auth\AuthRequest;
+use Illuminate\Http\Request;
+
+interface AuthInterfaces
+{
+    public function login(AuthRequest $request);
+    public function register(AuthRequest $request);
+    public function logout(Request $request);
+}
