cdb9ad67a47db6777f08e1a974a10d1e29179345
MRizki28 <muhammadrizkitkj123@gmail.com>
2025-02-22 21:30:14 +0800

    update

---
+++ app/Http/Requests/Auth/AuthRequest.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Http\Requests\Auth;
+
+use Illuminate\Contracts\Validation\Validator;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Http\Exceptions\HttpResponseException;
+
+class AuthRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        $rules = [];
+        if($this->is('api/v1/auth/login')) {
+            $rules = [
+                'email' => 'required|email',
+                'password' => 'required|string',
+            ];
+        } elseif($this->is('api/v1/auth/register')) {
+            $rules = [
+                'name' => 'required|string',
+                'email' => 'required|email|unique:users,email',
+                'password' => 'required|string',
+                'phone' => 'required|string',
+                'address' => 'required|string',
+            ];
+        }
+        return $rules;
+    }
+
+    protected function failedValidation(Validator $validator)
+    {
+        throw new HttpResponseException(response()->json([
+            'status' => "not validate",
+            'message' => 'Check your validation',
+            'data' => $validator->errors(),
+        ], 422));
+    }
+}
