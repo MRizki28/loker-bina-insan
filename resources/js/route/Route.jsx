9f9fa9dc8bc16b9c5c50f634062afc77276b6fd6
MRizki28 <muhammadrizkitkj123@gmail.com>
2025-02-23 13:00:23 +0800

    feat:authentication

--- resources/js/route/Route.jsx
+++ resources/js/route/Route.jsx
@@ -1,11 +1,13 @@
 import { Route, Routes } from "react-router-dom";
 import Home from "../pages/home/home";
+import ProtectedLoginRoutes from "../utils/protectedLoginRoutes";
+import Login from "../pages/auth/login";
 
 export default function AppRoute() {
     return (
         <Routes>
             <Route path="/" element={<Home></Home>}></Route>
-            
+            <Route path="/login" element={<ProtectedLoginRoutes><Login></Login></ProtectedLoginRoutes>}></Route>
         </Routes>
     )
 }
