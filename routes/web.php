fa260c07916b4f0b355d826798dc7c15a4db6d7e
MRizki28 <muhammadrizkitkj123@gmail.com>
2025-02-23 13:00:53 +0800

    feat:authentication

--- routes/web.php
+++ routes/web.php
@@ -2,9 +2,15 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/admin/loker', function () {
+Route::get('cms/admin/dashboard', function () {
     return view('pages.loker');
 });
+
+Route::get('/login' , function() {
+    return view('frontend');
+})->name('login');
+
+
 Route::fallback(function () {
     return view('frontend');
 });
