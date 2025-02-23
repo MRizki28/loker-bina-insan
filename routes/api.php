f920846bab2e6104a1344f69dd757fda0d0b840a
MRizki28 <muhammadrizkitkj123@gmail.com>
2025-02-23 13:00:48 +0800

    feat:authentication

--- routes/api.php
+++ routes/api.php
@@ -9,9 +9,18 @@ Route::get('/user', function (Request $request) {
 })->middleware('auth:sanctum');
 
 Route::prefix('v1')->group(function () {
-    Route::prefix('auth')->controller(AuthController::class)->group(function() {
+    Route::prefix('auth')->controller(AuthController::class)->group(function () {
         Route::post('login', 'login');
         Route::post('register', 'register');
-        Route::post('logout', 'logout');
+    });
+
+    Route::prefix('auth')->group(function () {
+        Route::middleware('auth:sanctum')->group(function () {
+            Route::post('logout', [AuthController::class, 'logout']);
+        });
     });
 });
+
+Route::get('v1/check-auth', function () {
+    return response()->json(['data' => true]);
+})->middleware('auth:sanctum');
