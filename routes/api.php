dbee52d1b762220deb45dcee0e3f69c8f09bccba
MRizki28 <muhammadrizkitkj123@gmail.com>
2025-03-01 11:29:02 +0800

    update

--- routes/api.php
+++ routes/api.php
@@ -8,19 +8,6 @@ Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
-Route::prefix('v1')->group(function () {
-    Route::prefix('auth')->controller(AuthController::class)->group(function () {
-        Route::post('login', 'login');
-        Route::post('register', 'register');
-    });
-
-    Route::prefix('auth')->group(function () {
-        Route::middleware('auth:sanctum')->group(function () {
-            Route::post('logout', [AuthController::class, 'logout']);
-        });
-    });
-});
-
 Route::get('v1/check-auth', function () {
     return response()->json(['data' => true]);
 })->middleware('auth:sanctum');
