ab67d7a050fcdd0191040cd59033a7cdf786ce1b
MRizki28 <muhammadrizkitkj123@gmail.com>
2025-03-01 11:28:58 +0800

    update

--- routes/web.php
+++ routes/web.php
@@ -1,16 +1,29 @@
 <?php
 
+use App\Http\Controllers\CMS\AuthController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('cms/admin/dashboard', function () {
-    return view('pages.loker');
-});
 
-Route::get('/login' , function() {
+Route::get('/login', function () {
     return view('frontend');
 })->name('login');
 
-
 Route::fallback(function () {
     return view('frontend');
+});
+
+
+Route::prefix('v1')->group(function () {
+    Route::prefix('auth')->controller(AuthController::class)->group(function () {
+        Route::post('login', 'login');
+        Route::post('register', 'register');
+    });
+});
+
+Route::post('v1/auth/logout', [AuthController::class, 'logout']);
+
+Route::middleware(['auth', 'web', 'adminRole'])->group(function () {
+    Route::get('cms/admin/dashboard', function () {
+        return view('pages.loker');
+    });
 });
