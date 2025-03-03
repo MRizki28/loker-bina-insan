f0fa2ae2834d1cf40fcb7c94188f62b89909f8cd
MRizki28 <muhammadrizkitkj123@gmail.com>
2025-03-03 13:25:24 +0800

    feat:job

---
+++ app/Models/JobModel.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class JobModel extends Model
+{
+    use HasFactory, HasUuids;
+
+    protected $table = 'tb_job';    
+
+    protected $fillable = [
+        'id',
+        'name',
+        'description',
+        'qualification',
+        'requirement',
+        'start_date',
+        'end_date',
+        'job_type',
+        'category',
+    ];
+
+    protected $casts = [
+        'qualification' => 'array',
+        'requirement' => 'array',
+    ];
+}
