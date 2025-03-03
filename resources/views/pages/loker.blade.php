e945cb84b7ad97cdb7a44a44bd65664d7b24fd30
MRizki28 <muhammadrizkitkj123@gmail.com>
2025-03-03 13:27:13 +0800

    feat:job

--- resources/views/pages/loker.blade.php
+++ resources/views/pages/loker.blade.php
@@ -1,3 +1,27 @@
 @extends('Layouts.Base')
 @section('content')
+    <div class="page-inner">
+        <x-base-header headerName="Data Hero" headerIcon="fas fa-file-image"></x-base-header>
+        <x-base-body>
+            <x-base-sorting addSearching="true" addNotificationAll="false" addUpdateStatus="false"
+                modalUpdateStatus="#updateStatusModal" buttonAdd="true" headerAddButton="Tambah Data"
+                modalId="#lokerModal"></x-base-sorting>
+            <x-base-table initId="table">
+                <x-slot name="thead">
+                    <tr class="text-center">
+                        <th>Judul</th>
+                        <th>Gambar Versi Desktop</th>
+                        <th>Gambar Versi Mobile</th>
+                        <th>Aksi</th>
+                    </tr>
+                </x-slot>
+            </x-base-table>
+        </x-base-body>
+    </div>
+    </div>
+    <x-loker.loker-modal></x-loker.loker-modal>
+@endsection
+
+@section('js-service')
+    <script type="module" src="{{ asset('js/job.controller.js') }}?v={{ time() }}"></script>
 @endsection
--- routes/api.php
+++ routes/api.php
@@ -8,6 +8,11 @@ Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
-Route::get('v1/check-auth', function () {
-    return response()->json(['data' => true]);
-})->middleware('auth:sanctum');
+// Route::get('v1/check-auth', function (Request $request) {
+//     $user = $request->user(); // Ambil user dari token Sanctum
+    
+//     return response()->json([
+//         'authenticated' => $user ? true : false,
+//         'user' => $user, // Bisa dikomentari jika tidak ingin mengembalikan data user
+//     ]);
+// })->middleware('auth:sanctum');
