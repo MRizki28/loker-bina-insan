c36c54f6848f8c0bcf3033cce004ea57f8780386
MRizki28 <muhammadrizkitkj123@gmail.com>
2025-03-03 13:25:13 +0800

    feat:job

---
+++ app/Http/Requests/Job/JobRequest.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Http\Requests\Job;
+
+use Illuminate\Contracts\Validation\Validator;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Http\Exceptions\HttpResponseException;
+
+class JobRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'name' => 'required|string',
+            'description' => 'required|string',
+            'requirement' => 'required',
+            'requirement.*' => 'required',
+            'qualification' => 'required',
+            'qualification.*' => 'required',
+            'start_date' => 'required|date',
+            'end_date' => 'required|date',
+            'job_type' => 'required',
+            'category' => 'required',
+        ];
+    }
+
+    protected function failedValidation(Validator $validator)
+    {
+        throw new HttpResponseException(response()->json([
+            'status' => "not validate",
+            'message' => 'Check your validation',
+            'data' => $validator->errors(),
+        ], 422));
+    }
+}
