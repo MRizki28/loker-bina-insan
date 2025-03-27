976220904eee971f0ac9a5932d5b3d92bd5ad764
Rizki <muhammadrizkitkj123@gmail.com>
2025-03-27 16:26:11 +0800

    update

--- resources/views/pages/loker.blade.php
+++ resources/views/pages/loker.blade.php
@@ -1,7 +1,7 @@
 @extends('Layouts.Base')
 @section('content')
     <div class="page-inner">
-        <x-base-header headerName="Data Hero" headerIcon="fas fa-file-image"></x-base-header>
+        <x-base-header headerName="Data loker" headerIcon="fas fa-file-image"></x-base-header>
         <x-base-body>
             <x-base-sorting addSearching="true" addNotificationAll="false" addUpdateStatus="false"
                 modalUpdateStatus="#updateStatusModal" buttonAdd="true" headerAddButton="Tambah Data"
@@ -9,14 +9,57 @@
             <x-base-table initId="table">
                 <x-slot name="thead">
                     <tr class="text-center">
-                        <th>Judul</th>
-                        <th>Gambar Versi Desktop</th>
-                        <th>Gambar Versi Mobile</th>
+                        <th>Lowongan</th>
+                        <th>Deskripsi</th>
+                        <th>Kualifikasi</th>
+                        <th>Requirement</th>
+                        <th>Waktu lowongan</th>
+                        <th>Type</th>
+                        <th>Kategory</th>
                         <th>Aksi</th>
                     </tr>
                 </x-slot>
             </x-base-table>
         </x-base-body>
+    </div>
+    <div class="modal fade" id="qualificationModal" tabindex="-1" role="dialog" aria-labelledby="qualificationLabel"
+        aria-hidden="true">
+        <div class="modal-dialog" role="document" style="max-width: 800px;">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="modal-title">Kualifikasi</h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <div class="modal-body">
+                    <ol id="qualificationList" style="color: #000;"></ol>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="modal fade" id="requirementModal" tabindex="-1" role="dialog" aria-labelledby="requirementLabel"
+        aria-hidden="true">
+        <div class="modal-dialog" role="document" style="max-width: 800px;">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="modal-title">Pesyaratan</h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <div class="modal-body">
+                    <ol id="requirementList" style="color: #000;"></ol>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                </div>
+            </div>
+        </div>
     </div>
     </div>
     <x-loker.loker-modal></x-loker.loker-modal>
