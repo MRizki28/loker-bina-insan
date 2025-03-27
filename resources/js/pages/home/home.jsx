cb82e67cdcc393a1e5ae531404ac46b78f6f4098
Rizki <muhammadrizkitkj123@gmail.com>
2025-03-27 16:26:07 +0800

    update

--- resources/js/pages/home/home.jsx
+++ resources/js/pages/home/home.jsx
@@ -1,5 +1,7 @@
+import About from "../../components/home/about";
 import Content from "../../components/home/content";
 import Hero from "../../components/home/hero";
+import Lingkungan from "../../components/home/lingkungan";
 import Footer from "../../components/shared/Footer";
 import Navbar from "../../components/shared/Navbar";
 
@@ -7,8 +9,10 @@ export default function Home() {
     return (
         <div className="flex flex-col min-h-screen">
             <Navbar />
-            <main className="flex-grow">
+            <main className="flex-grow bg-gray-50">
                 <Hero></Hero>
+                <About></About>
+                <Lingkungan></Lingkungan>
                 <Content></Content>
             </main>
             <Footer />
