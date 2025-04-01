47a452f435dfcbd0dc4798683ee919f687635b88
MRizki28 <muhammadrizkitkj123@gmail.com>
2025-04-01 20:26:46 +0800

    update

--- resources/views/Layouts/Sidebar.blade.php
+++ resources/views/Layouts/Sidebar.blade.php
@@ -4,7 +4,7 @@
 				<div class="sidebar-content">
 					<div class="user">
 						<div class="avatar-sm float-left mr-2">
-							<img src="{{asset('img/profilepeople.png')}}" alt="" class="avatar-img rounded-circle">
+							<img src="{{asset('static/img/profile.png')}}" alt="" class="avatar-img rounded-circle">
 						</div>
 						<div class="info">
 							<a href="{{ url('/setting') }}" >
