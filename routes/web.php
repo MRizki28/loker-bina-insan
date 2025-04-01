3a39351c0cd64849db30319aa0b2c059099a9ade
MRizki28 <muhammadrizkitkj123@gmail.com>
2025-04-01 20:26:51 +0800

    update

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\CMS\AuthController;
+use App\Http\Controllers\CMS\FileApplyController;
 use App\Http\Controllers\CMS\JobController;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
@@ -17,7 +18,7 @@ Route::fallback(function () {
 
 Route::get('v1/check-auth', function () {
     $user = Auth::user();
-    
+
     return response()->json([
         'authenticated' => $user ? true : false,
         'user' => $user
@@ -29,11 +30,18 @@ Route::prefix('v1')->group(function () {
         Route::post('login', 'login');
         Route::post('register', 'register');
     });
+
+    Route::prefix('job')->controller(JobController::class)->group(function () {
+        Route::get('/get-for-frontend', 'getForFrontend');
+        Route::get('/get-for-frontend/get/{id}', 'getDataById');
+    });
+
+
 });
 
 Route::post('v1/auth/logout', [AuthController::class, 'logout']);
 
-Route::middleware(['adminRole'])->group(function () {
+Route::middleware(['auth', 'web', 'adminRole'])->group(function () {
     Route::get('cms/admin/dashboard', function () {
         return view('pages.loker');
     });
@@ -50,5 +58,10 @@ Route::middleware(['adminRole'])->group(function () {
             Route::post('/update/{id}', 'updateData');
             Route::delete('/delete/{id}', 'deleteData');
         });
+        Route::prefix('file-apply')->controller(FileApplyController::class)->group(function () {
+            Route::post('/create', 'createData');
+        });
     });
+
+   
 });
