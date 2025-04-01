25ed649949158ce6c5e851756eadf168a1aee04f
MRizki28 <muhammadrizkitkj123@gmail.com>
2025-04-01 22:00:01 +0800

    update

--- resources/js/redux/store.js
+++ resources/js/redux/store.js
@@ -1,16 +1,20 @@
 import { configureStore } from "@reduxjs/toolkit";
-import checkLogin from "./slices/checkLogin";
+import checkLoginReducer from "./slices/checkLogin";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
+
+const persistConfig = {
+    key: "root",
+    storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, checkLoginReducer);
 
 const store = configureStore({
-    reducer:{
-        checkLogin: checkLogin
-    }
-})
+    reducer: {
+        checkLogin: persistedReducer,
+    },
+});
 
-console.log(store.getState())
-
-store.subscribe(() => {
-    store.getState()
-})
-
-export default store
+export const persistor = persistStore(store);
+export default store;
