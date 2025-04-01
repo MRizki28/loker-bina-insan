fd4bca76016b6188ae5104b46bb80791a2eaf4ff
MRizki28 <muhammadrizkitkj123@gmail.com>
2025-04-01 21:58:29 +0800

    update

--- app/Models/FileApplyModel.php
+++ app/Models/FileApplyModel.php
@@ -2,10 +2,13 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class FileApplyModel extends Model
 {
+    use HasFactory, HasUuids;
     protected $table = 'tb_file';
     protected $fillable = [
         'id',
