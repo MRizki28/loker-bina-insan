e49c8f9181525df1c2062fdcf77daa1813c68b3b
MRizki28 <muhammadrizkitkj123@gmail.com>
2025-04-01 20:25:02 +0800

    update

---
+++ app/Http/Requests/FileApply/FileApplyRequest.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Http\Requests\FileApply;
+
+use Illuminate\Contracts\Validation\Validator;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Http\Exceptions\HttpResponseException;
+
+class FileApplyRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'id_job' => 'required|uuid|exists:tb_job,id',
+            'file' => 'required|file|mimes:pdf,zip,rar',
+            'reason' => 'required|string',
+        ];
+    }
+
+    protected function failedValidation(Validator $validator)
+    {
+        throw new HttpResponseException(response()->json([
+            'status' => "not validate",
+            'message' => 'Check your validation',
+            'data' => $validator->errors(),
+        ], 422));
+    }
+}
