c10c9a0c84e35cd92f84570872b1e70f5d5f29c3
MRizki28 <muhammadrizkitkj123@gmail.com>
2025-04-02 19:56:39 +0800

    update

--- resources/js/route/Route.jsx
+++ resources/js/route/Route.jsx
@@ -4,6 +4,7 @@ import ProtectedLoginRoutes from "../utils/protectedLoginRoutes";
 import Login from "../pages/auth/login";
 import Register from "../pages/auth/register";
 import DetailLoker from "../pages/detailLoker/detailLoker";
+import History from "../pages/history/history";
 
 export default function AppRoute() {
     return (
@@ -13,6 +14,7 @@ export default function AppRoute() {
             <Route path="/register" element={<Register></Register>}></Route>
             <Route path="*" element={<h1>404 Not Found</h1>}></Route>
             <Route path="/detail/:id" element={<DetailLoker></DetailLoker>}></Route>
+            <Route path="/history" element={<History></History>}></Route>
         </Routes>
     )
 }
