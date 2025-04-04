f0615d7bc315134b9ebe42bc6ad02e1236fe81da
MRizki28 <muhammadrizkitkj123@gmail.com>
2025-04-04 20:12:46 +0800

    update

---
+++ resources/views/pages/fileapply.blade.php
@@ -0,0 +1,67 @@
+@extends('Layouts.Base')
+@section('content')
+    <div class="page-inner">
+        <x-base-header headerName="Data loker" headerIcon="fas fa-file-image"></x-base-header>
+        <x-base-body>
+            <x-base-sorting addSearching="true" addNotificationAll="false" addUpdateStatus="false"
+                modalUpdateStatus="#updateStatusModal" buttonAdd="false" headerAddButton="Tambah Data"
+                modalId="#reviewModal"></x-base-sorting>
+            <x-base-table initId="table">
+                <x-slot name="thead">
+                    <tr class="text-center">
+                        <th>Nama pelamar</th>
+                        <th>Job yang dilamar</th>
+                        <th>Status review</th>
+                        <th>Alasan Penolakan</th>
+                        <th>Aksi</th>
+                    </tr>
+                </x-slot>
+            </x-base-table>
+        </x-base-body>
+    </div>
+    <div class="modal fade" id="qualificationModal" tabindex="-1" role="dialog" aria-labelledby="qualificationLabel"
+        aria-hidden="true">
+        <div class="modal-dialog" role="document" style="max-width: 800px;">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="modal-title">Kualifikasi</h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <div class="modal-body">
+                    <ol id="qualificationList" style="color: #000;"></ol>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="modal fade" id="requirementModal" tabindex="-1" role="dialog" aria-labelledby="requirementLabel"
+        aria-hidden="true">
+        <div class="modal-dialog" role="document" style="max-width: 800px;">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="modal-title">Pesyaratan</h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <div class="modal-body">
+                    <ol id="requirementList" style="color: #000;"></ol>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                </div>
+            </div>
+        </div>
+    </div>
+    </div>
+    <x-review.review-modal></x-review.review-modal>
+@endsection
+
+@section('js-service')
+    <script type="module" src="{{ asset('js/fileapply.controller.js') }}?v={{ time() }}"></script>
+@endsection
