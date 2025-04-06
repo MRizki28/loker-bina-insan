ee981df97eb1545091b54ae3292bfd82593dfad9
MRizki28 <muhammadrizkitkj123@gmail.com>
2025-04-06 18:46:25 +0800

    feat:notif email

--- public/js/fileapply.controller.js
+++ public/js/fileapply.controller.js
@@ -8,4 +8,39 @@ $(document).ready(function () {
         const id = $(this).data('id')
         fileApplyService.getReview(id)
     })
+
+    $(document).on('click', '#rejectBtn', function (e) {
+        e.preventDefault();
+        
+        const id = $(this).data('id');
+        $('#rejectModal').modal('show');
+        $('#reviewModal').modal('hide');
+    
+        $('#formReject #id').val(id);
+    });
+    
+
+    $(document).on('submit', '#formReject', function (e) {
+        e.preventDefault();
+    
+        const id = $('#formReject #id').val();
+        const reason = $('#formReject #reason_reject').val();
+    
+        // Validasi singkat (opsional)
+        if (!reason) {
+            alert('Harap isi alasan penolakan!');
+            return;
+        }
+    
+        // Kirim data via axios
+        fileApplyService.reject(id, reason, e);
+    });
+
+    $(document).on('click', '.approveBtn', function (e) {
+        e.preventDefault();
+        const id = $(this).data('id');
+        fileApplyService.approve(id, e);
+    });
+    
+    
 });
