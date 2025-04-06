4753a0f6b751d147b8f6c1889defb526e9e6f310
MRizki28 <muhammadrizkitkj123@gmail.com>
2025-04-06 18:46:42 +0800

    feat:notif email

--- resources/views/email/loker.blade.php
+++ resources/views/email/loker.blade.php
@@ -7,9 +7,6 @@
   <title>Email Confirmation</title>
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <style type="text/css">
-  /**
-   * Google webfonts. Recommended to include the .woff version for cross-client compatibility.
-   */
   @media screen {
     @font-face {
       font-family: 'Source Sans Pro';
@@ -26,38 +23,24 @@
     }
   }
 
-  /**
-   * Avoid browser level font resizing.
-   * 1. Windows Mobile
-   * 2. iOS / OSX
-   */
   body,
   table,
   td,
   a {
-    -ms-text-size-adjust: 100%; /* 1 */
-    -webkit-text-size-adjust: 100%; /* 2 */
+    -ms-text-size-adjust: 100%;
+    -webkit-text-size-adjust: 100%;
   }
 
-  /**
-   * Remove extra space added to tables and cells in Outlook.
-   */
   table,
   td {
     mso-table-rspace: 0pt;
     mso-table-lspace: 0pt;
   }
 
-  /**
-   * Better fluid images in Internet Explorer.
-   */
   img {
     -ms-interpolation-mode: bicubic;
   }
 
-  /**
-   * Remove blue links for iOS devices.
-   */
   a[x-apple-data-detectors] {
     font-family: inherit !important;
     font-size: inherit !important;
@@ -67,9 +50,6 @@
     text-decoration: none !important;
   }
 
-  /**
-   * Fix centering issues in Android 4.4.
-   */
   div[style*="margin: 16px 0;"] {
     margin: 0 !important;
   }
@@ -81,9 +61,6 @@
     margin: 0 !important;
   }
 
-  /**
-   * Collapse table borders to avoid space between cells.
-   */
   table {
     border-collapse: collapse !important;
   }
@@ -104,49 +81,25 @@
 </head>
 <body style="background-color: #e9ecef;">
 
-  <!-- start preheader -->
   <div class="preheader" style="display: none; max-width: 0; max-height: 0; overflow: hidden; font-size: 1px; line-height: 1px; color: #fff; opacity: 0;">
     A preheader is the short summary text that follows the subject line when an email is viewed in the inbox.
   </div>
-  <!-- end preheader -->
 
-  <!-- start body -->
   <table border="0" cellpadding="0" cellspacing="0" width="100%">
 
-    <!-- start logo -->
     <tr>
       <td align="center" bgcolor="#e9ecef">
-        <!--[if (gte mso 9)|(IE)]>
-        <table align="center" border="0" cellpadding="0" cellspacing="0" width="600">
-        <tr>
-        <td align="center" valign="top" width="600">
-        <![endif]-->
         <table border="0" cellpadding="0" cellspacing="0" width="100%" style="max-width: 600px;">
           <tr>
             <td align="center" valign="top" style="padding: 36px 24px;">
-              
-             
-              
             </td>
           </tr>
         </table>
-        <!--[if (gte mso 9)|(IE)]>
-        </td>
-        </tr>
-        </table>
-        <![endif]-->
       </td>
     </tr>
-    <!-- end logo -->
 
-    <!-- start hero -->
     <tr>
       <td align="center" bgcolor="#e9ecef">
-        <!--[if (gte mso 9)|(IE)]>
-        <table align="center" border="0" cellpadding="0" cellspacing="0" width="600">
-        <tr>
-        <td align="center" valign="top" width="600">
-        <![endif]-->
         <table border="0" cellpadding="0" cellspacing="0" width="100%" style="max-width: 600px;">
           <tr>
             <td align="left" bgcolor="#ffffff" style="padding: 36px 24px 0; font-family: 'Source Sans Pro', Helvetica, Arial, sans-serif; border-top: 3px solid #d4dadf;">
@@ -154,97 +107,56 @@
             </td>
           </tr>
         </table>
-        <!--[if (gte mso 9)|(IE)]>
-        </td>
-        </tr>
-        </table>
-        <![endif]-->
       </td>
     </tr>
-    <!-- end hero -->
 
-    <!-- start copy block -->
     <tr>
       <td align="center" bgcolor="#e9ecef">
-        <!--[if (gte mso 9)|(IE)]>
-        <table align="center" border="0" cellpadding="0" cellspacing="0" width="600">
-        <tr>
-        <td align="center" valign="top" width="600">
-        <![endif]-->
         <table border="0" cellpadding="0" cellspacing="0" width="100%" style="max-width: 600px;">
 
-          <!-- start copy -->
           <tr>
             <td align="left" bgcolor="#ffffff" style="padding: 24px; font-family: 'Source Sans Pro', Helvetica, Arial, sans-serif; font-size: 16px; line-height: 24px;">
-              <p style="margin: 0;" ></p>
+              <p style="margin: 0;">{{ $bodyMessage }}</p>
             </td>
           </tr>
-          <!-- end copy -->
 
-
-          <!-- start copy -->
           <tr>
             <td align="left" bgcolor="#ffffff" style="padding: 24px; font-family: 'Source Sans Pro', Helvetica, Arial, sans-serif; font-size: 16px; line-height: 24px;">
-              <p style="margin: 0;">Jaga kesahatan dan jangan lupa senyum hari ini </p>
-  
+              <p style="margin: 0;">Tetap jaga kesehatan Anda, karena hanya guru yang sehat yang mampu menginspirasi dengan sepenuh hati.</p>
             </td>
           </tr>
-          <!-- end copy -->
 
-          <!-- start copy -->
+          <!-- start auto email note -->
+          <tr>
+            <td align="left" bgcolor="#ffffff" style="padding: 24px; font-family: 'Source Sans Pro', Helvetica, Arial, sans-serif; font-size: 14px; line-height: 20px; color: #666;">
+              <p style="margin: 0;"><em>Email ini dikirim secara otomatis oleh sistem. Mohon untuk tidak membalas email ini.</em></p>
+            </td>
+          </tr>
+          <!-- end auto email note -->
+
           <tr>
             <td align="left" bgcolor="#ffffff" style="padding: 24px; font-family: 'Source Sans Pro', Helvetica, Arial, sans-serif; font-size: 16px; line-height: 24px; border-bottom: 3px solid #d4dadf">
-              <p style="margin: 0;">Admin <br> Kelurahan kawatuna   </p>
+              <p style="margin: 0;">STN BINA INSAN PALU<br></p>
             </td>
           </tr>
-          <!-- end copy -->
 
         </table>
-        <!--[if (gte mso 9)|(IE)]>
-        </td>
-        </tr>
-        </table>
-        <![endif]-->
       </td>
     </tr>
-    <!-- end copy block -->
 
-    <!-- start footer -->
     <tr>
       <td align="center" bgcolor="#e9ecef" style="padding: 24px;">
-        <!--[if (gte mso 9)|(IE)]>
-        <table align="center" border="0" cellpadding="0" cellspacing="0" width="600">
-        <tr>
-        <td align="center" valign="top" width="600">
-        <![endif]-->
         <table border="0" cellpadding="0" cellspacing="0" width="100%" style="max-width: 600px;">
-
-          <!-- start permission -->
           <tr>
             <td align="center" bgcolor="#e9ecef" style="padding: 12px 24px; font-family: 'Source Sans Pro', Helvetica, Arial, sans-serif; font-size: 14px; line-height: 20px; color: #666;">
-              <p style="margin: 0;">@Keluarahan Kawatuna</p>
+              <p style="margin: 0;">@STN BINA INSAN PALU</p>
             </td>
           </tr>
-          <!-- end permission -->
-
-          <!-- start unsubscribe -->
-          <tr>
-          
-          </tr>
-          <!-- end unsubscribe -->
-
         </table>
-        <!--[if (gte mso 9)|(IE)]>
-        </td>
-        </tr>
-        </table>
-        <![endif]-->
       </td>
     </tr>
-    <!-- end footer -->
 
   </table>
-  <!-- end body -->
 
 </body>
 </html>
