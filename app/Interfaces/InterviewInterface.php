9a754f3de43f3fc7f1c8719e79137402f1d75753
MRizki28 <muhammadrizkitkj123@gmail.com>
2025-04-09 06:40:10 +0800

    update

---
+++ app/Interfaces/InterviewInterface.php
@@ -0,0 +1,15 @@
+<?php
+
+
+namespace App\Interfaces;
+
+use Illuminate\Http\Request;
+
+interface InterviewInterface
+{
+    public function getAllData($request);
+    public function getDataById($id);
+    public function getHistoryByUser($request);
+    public function review(Request $request, $id);
+
+}
