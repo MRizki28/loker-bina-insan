77865ad405f3d2aa0f2df7a445d3c226f950834b
MRizki28 <muhammadrizkitkj123@gmail.com>
2025-04-13 21:34:58 +0800

    update

---
+++ app/Repositories/ArchiveRepositories.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Repositories;
+
+use App\Interfaces\ArchiveInterfaces;
+use App\Models\ArchiveModel;
+use Illuminate\Support\Facades\Auth;
+use MRizki28\ApiResponse\ApiResponse;
+
+class ArchiveRepositories implements ArchiveInterfaces {
+    protected $archiveModel;
+
+    public function __construct(ArchiveModel $archiveModel)
+    {
+        $this->archiveModel = $archiveModel;
+    }
+
+    public function getAllData($request)
+    {
+        
+    }
+
+    public function getDataById($id)
+    {
+        
+    }
+
+    public function getHistoryByUser($request)
+    {
+        try {
+            $search = $request->input('search');
+            $limit = $request->input('limit') ? $request->input('limit') : 10;
+            $page = $search ? 1 : (int) $request->input('page', 1);
+            $query = $this->archiveModel->query();
+
+            if($search){
+                $query->where('status', 'like', '%'.$search.'%');
+            }
+
+            $data = $query->where('id_pelamar', Auth::user()->id)->with('pelamar')->paginate($limit, ['*'], 'page', $page);
+
+            if($data->isEmpty()){
+                return ApiResponse::notFound();
+            }
+
+            return ApiResponse::success($data, 'Success get data job', 200);
+        } catch (\Throwable $th) {
+            return ApiResponse::error($th);
+            
+        }
+    }
+
+    public function review($request, $id)
+    {
+        
+    }
+    
+}
