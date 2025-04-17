af4b9396514910ee1d659efe17168a41d5e8782a
MRizki28 <muhammadrizkitkj123@gmail.com>
2025-04-17 16:43:36 +0800

    update

--- app/Repositories/AuthRepositories.php
+++ app/Repositories/AuthRepositories.php
@@ -23,7 +23,7 @@ class AuthRepositories implements AuthInterfaces
     {
         try {
             $user = $this->userModel->where('email', $request->email)->first();
-            
+
             if (!$user || !Auth::attempt($request->only('email', 'password'))) {
                 return ApiResponse::unauthorized();
             }
@@ -59,16 +59,16 @@ class AuthRepositories implements AuthInterfaces
     {
         try {
             $user = $request->user() ?? Auth::user();
-    
+
             if ($user) {
-                $user->tokens()->delete(); 
+                $user->tokens()->delete();
             }
-    
+
             Auth::guard('web')->logout();
-    
+
             $request->session()->invalidate();
             $request->session()->regenerateToken();
-    
+
             return response()->json([
                 'status' => 'success',
                 'message' => 'Logout successful',
@@ -76,5 +76,24 @@ class AuthRepositories implements AuthInterfaces
         } catch (\Throwable $th) {
             return ApiResponse::error($th, 500);
         }
+    }
+
+    public function getDataUser(Request $request)
+    {
+        $search = $request->input('search');
+        $limit = $request->input('limit') ? $request->input('limit') : 10;
+        $page = $search ? 1 : (int) $request->input('page', 1);
+
+        $query = $this->userModel->query();
+
+        if($search){
+            $query->where('name', 'like', '%'.$search.'%');
+        }
+        $data = $query->where('role', 'user')->paginate($limit, ['*'], 'page', $page);
+
+        if($data->isEmpty()){
+            return ApiResponse::notFound();
+        }
+        return ApiResponse::success($data, 'Success get data user', 200);
     }
 }
