deb21c82b80945a277b4d50ccc3d5e2eec8d33fc
MRizki28 <muhammadrizkitkj123@gmail.com>
2025-04-17 16:43:46 +0800

    update

---
+++ public/js/archive.controller.js
@@ -0,0 +1,26 @@
+import ArchiveService from "./archive.service.js?v=1.0.0";
+
+$(document).ready(function () {
+    const archiveService = new ArchiveService()
+    archiveService.getAllData()
+    $(document).on('click', '.page-link', function (e) {
+        e.preventDefault();
+        const url = new URL($(this).attr('href'));
+        console.log(url)
+        const fullUrl = url.pathname + url.search;
+        archiveService.getAllData(fullUrl);
+    });
+
+    $('#data-pengguna').off('click', '.btn-user').on('click', '.btn-user', function () {
+        const name = $(this).data('name');
+        const id = $(this).data('id');
+        console.log(id);
+        $('#name-pengguna').text(name);
+        archiveService.getDataByPelamar(id);
+
+        $(document).on('click', '#search-button', function () {
+            archiveService.getDataByPelamar(id);
+        })
+    });
+    
+});
