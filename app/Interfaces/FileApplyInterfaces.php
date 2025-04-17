d500e17769f34e28e12e55af032d344ea4a543bc
MRizki28 <muhammadrizkitkj123@gmail.com>
2025-04-17 09:03:03 +0800

    update

--- app/Interfaces/FileApplyInterfaces.php
+++ app/Interfaces/FileApplyInterfaces.php
@@ -16,5 +16,4 @@ interface FileApplyInterfaces
     public function getHistoryByUser(Request $request);
     public function downloadFile($filename);
     public function reviewFile(Request $request, $id);
-    public function getDataInterview(Request $request, $id_pelamar);
 }
