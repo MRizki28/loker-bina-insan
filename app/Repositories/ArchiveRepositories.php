b614d8cb419f213572aebc97c0e7f1c60a94b43f
MRizki28 <muhammadrizkitkj123@gmail.com>
2025-04-17 16:43:33 +0800

    update

--- app/Repositories/ArchiveRepositories.php
+++ app/Repositories/ArchiveRepositories.php
@@ -4,6 +4,7 @@ namespace App\Repositories;
 
 use App\Interfaces\ArchiveInterfaces;
 use App\Models\ArchiveModel;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use MRizki28\ApiResponse\ApiResponse;
 
@@ -25,16 +26,21 @@ class ArchiveRepositories implements ArchiveInterfaces {
         
     }
 
-    public function getHistoryByUser($request)
+    public function getHistoryByUser(Request $request)
     {
         try {
             $search = $request->input('search');
             $limit = $request->input('limit') ? $request->input('limit') : 10;
             $page = $search ? 1 : (int) $request->input('page', 1);
+            $status = $request->input('status');    
             $query = $this->archiveModel->query();
 
             if($search){
                 $query->where('status', 'like', '%'.$search.'%');
+            }
+
+            if($status){
+                $query->where('status', $status);
             }
 
             $data = $query->where('id_pelamar', Auth::user()->id)->with('pelamar')->paginate($limit, ['*'], 'page', $page);
@@ -53,6 +59,29 @@ class ArchiveRepositories implements ArchiveInterfaces {
     public function review($request, $id)
     {
         
+    }
+
+    public function getDataByPelamar(Request $request , $id)
+    {
+        $search = $request->input('search');
+        $page = $search ? 1 : (int) $request->input('page', 1);
+        $limit = $request->input('limit') ? $request->input('limit') : 10;
+        $query = $this->archiveModel->query();
+
+        if ($search) {
+            $query->whereHas('pelamar', function ($q) use ($search) {
+                $q->where('name', 'like', '%' . $search . '%');
+            });
+        }
+        
+        $data = $query->where('id_pelamar', $id)->with('pelamar')
+            ->paginate($limit, ['*'], 'page', $page);
+
+        if ($data->isEmpty()) {
+            return ApiResponse::notFound();
+        }
+
+        return ApiResponse::success($data, 'Success get data job', 200);
     }
     
 }
