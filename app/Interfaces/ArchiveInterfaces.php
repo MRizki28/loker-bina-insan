d562f064703c4e13e913a9482d55570c7ebacd2a
MRizki28 <muhammadrizkitkj123@gmail.com>
2025-04-17 16:43:23 +0800

    update

--- app/Interfaces/ArchiveInterfaces.php
+++ app/Interfaces/ArchiveInterfaces.php
@@ -3,10 +3,13 @@
 
 namespace App\Interfaces;
 
+use Illuminate\Http\Request;
+
 interface ArchiveInterfaces
 {
     public function getAllData($request);
     public function getDataById($id);
-    public function getHistoryByUser($request);
+    public function getHistoryByUser(Request $request);
     public function review($request, $id);
+    public function getDataByPelamar(Request $request, $id);
 }
