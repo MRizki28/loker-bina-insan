4f7394dc85472f1bb65fdf787a4302199e599c1b
MRizki28 <muhammadrizkitkj123@gmail.com>
2025-04-18 23:54:07 +0800

    update

--- public/js/fileapply.service.js
+++ public/js/fileapply.service.js
@@ -7,64 +7,73 @@ class FileApplyService {
 
         let params = $('#form-search').val();
         let endpoint = paramsUrl(url || '/v1/file-apply', { search: params });
-        const response = await axios.get(endpoint);
-        const responseData = await response.data;
-        console.log('ini response', responseData)
 
         table.empty();
         pagination.empty();
 
+        try {
+            const response = await axios.get(endpoint);
+            const responseData = await response.data;
+            console.log('ini response', responseData)
 
-        let tableBody
-        if (responseData.message === 'Success get data job') {
-            $.each(responseData.data.data, function (index, item) {
-                let badgeHtml = '';
+            let tableBody
+            if (responseData.message === 'Success get data job') {
+                $.each(responseData.data.data, function (index, item) {
+                    let badgeHtml = '';
 
-                if (item.status == 'pending') {
-                    badgeHtml = '<span class="badge badge-warning">Sedang di review</span>';
-                } else if (item.status == 'rejected') {
-                    badgeHtml = '<span class="badge badge-danger">Ditolak</span>';
-                } else {
-                    badgeHtml = '<span class="badge badge-success">Approve lanjut ke tahap wawancara</span>';
-                }
+                    if (item.status == 'pending') {
+                        badgeHtml = '<span class="badge badge-warning">Sedang di review</span>';
+                    } else if (item.status == 'rejected') {
+                        badgeHtml = '<span class="badge badge-danger">Ditolak</span>';
+                    } else {
+                        badgeHtml = '<span class="badge badge-success">Approve lanjut ke tahap wawancara</span>';
+                    }
 
-                tableBody += "<tr>";
-                tableBody += "<td>" + item.pelamar.name + "</td>"
-                tableBody += "<td>" + item.job.name + "</td>"
-                // tableBody +=
-                //     "<td style='padding: 0 10px !important;'  class='text-center '>" +
-                //     "<button class='btn btn-sm qualification-modal mr-1' data-toggle='modal' data-target='#qualificationModal' data-id='" +
-                //     item.id + "'><i class='fas fa-eye'></i></button>"
-                // tableBody += "<td>" + item.reason + "</td>"
-                tableBody += "<td>" + badgeHtml + "</td>"
-                tableBody += "<td>" + (item.reason_reject ?? "-") + "</td>";
-                if (item.status === 'pending') {
-                    tableBody +=
-                        "<td style='padding: 0 10px !important;'  class='text-center '>" +
-                        "<button class='btn btn-sm review-modal mr-1' data-toggle='modal' data-target='#reviewModal' data-id='" +
-                        item.id + "'>Review</button>" +
-                        "<button type='submit' class='delete-confirm btn btn-sm' data-id='" +
-                        item.id + "'><i class='fas fa-trash-alt'></i></button>" +
-                        "</td>";
-                } else {
-                    tableBody +=
-                        "<td style='padding: 0 10px !important;'  class='text-center '>" +
-                        "<span>Sudah di review</span>" +
-                        "</td>";
-                }
-                tableBody += "</tr>";
-                dataNotFound.hide()
-            });
+                    tableBody += "<tr>";
+                    tableBody += "<td>" + item.pelamar.name + "</td>"
+                    tableBody += "<td>" + item.job.name + "</td>"
+                    // tableBody +=
+                    //     "<td style='padding: 0 10px !important;'  class='text-center '>" +
+                    //     "<button class='btn btn-sm qualification-modal mr-1' data-toggle='modal' data-target='#qualificationModal' data-id='" +
+                    //     item.id + "'><i class='fas fa-eye'></i></button>"
+                    // tableBody += "<td>" + item.reason + "</td>"
+                    tableBody += "<td>" + badgeHtml + "</td>"
+                    tableBody += "<td>" + (item.reason_reject ?? "-") + "</td>";
+                    if (item.status === 'pending') {
+                        tableBody +=
+                            "<td style='padding: 0 10px !important;'  class='text-center '>" +
+                            "<button class='btn btn-sm review-modal mr-1' data-toggle='modal' data-target='#reviewModal' data-id='" +
+                            item.id + "'>Review</button>" +
+                            "<button type='submit' class='delete-confirm btn btn-sm' data-id='" +
+                            item.id + "'><i class='fas fa-trash-alt'></i></button>" +
+                            "</td>";
+                    } else {
+                        tableBody +=
+                            "<td style='padding: 0 10px !important;'  class='text-center '>" +
+                            "<span>Sudah di review</span>" +
+                            "</td>";
+                    }
+                    tableBody += "</tr>";
+                    dataNotFound.hide()
+                });
 
-            table.append(tableBody);
-            paginationLink(pagination, responseData);
-            totalData.text(responseData.data.total);
-        } else {
+                table.append(tableBody);
+                paginationLink(pagination, responseData);
+                totalData.text(responseData.data.total);
+            } else {
+                table.empty()
+                dataNotFound.show()
+                pagination.empty()
+                totalData.text('0')
+            }
+
+        } catch (error) {
             table.empty()
             dataNotFound.show()
             pagination.empty()
             totalData.text('0')
         }
+
     }
 
     async getReview(id) {
@@ -96,7 +105,7 @@ class FileApplyService {
             $('#unduhFile').attr('href', filePath).attr('download', responseData.data.file);
             $('#approveBtn').attr('data-id', id);
             $('#rejectBtn').attr('data-id', id);
-            
+
 
         } catch (error) {
             console.log(error);
