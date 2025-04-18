7d8e6e31af468becb6a4e4146ef01b3ae7ef89cf
MRizki28 <muhammadrizkitkj123@gmail.com>
2025-04-18 23:54:11 +0800

    update

--- public/js/interview.service.js
+++ public/js/interview.service.js
@@ -7,88 +7,97 @@ class InterviewService {
 
         let params = $('#form-search').val();
         let endpoint = paramsUrl(url || '/v1/interview/', { search: params });
-        const response = await axios.get(endpoint);
-        const responseData = await response.data;
-        console.log('ini response', responseData)
 
         table.empty();
         pagination.empty();
+        
+        try {
+            let tableBody
 
+            const response = await axios.get(endpoint);
+            const responseData = await response.data;
+            console.log('ini response', responseData)
 
-        let tableBody
-        if (responseData.message === 'Success get data job') {
-            $.each(responseData.data.data, function (index, item) {
-                let badgeHtml = '';
-
-                if (item.status == 'pending') {
-                    badgeHtml = '<span class="badge badge-warning">Sedang di review</span>';
-                } else if (item.status == 'rejected') {
-                    badgeHtml = '<span class="badge badge-danger">Ditolak</span>';
-                } else {
-                    badgeHtml = '<span class="badge badge-success">Approve lanjut ke tahap wawancara</span>';
-                }
-
-                tableBody += "<tr>";
-                tableBody += "<td>" + item.file.pelamar.name + "</td>"
-                tableBody += "<td>" + item.file.job.name + "</td>"
-
-                tableBody += "<td>" + badgeHtml + "</td>"
-                if (item.time_interview == null) {
-                    tableBody += "<td>" + "-" + "</td>";
-                } else {
-                    tableBody += "<td>" + item.time_interview + "</td>";
-                }
-
-                if (item.link == null) {
-                    tableBody += "<td>" + "Interview offline" + "</td>";
-                } else {
-                    tableBody += "<td>" + item.link + "</td>";
-                }
-
-                if (item.status_interview == 'pending') {
-                    tableBody += "<td>" + "<span class='badge badge-warning'>Siap interview</span>" + "</td>"
-                }else if(item.status_interview == 'lolos') {
-                    tableBody += "<td>" + "<span class='badge badge-success'>Lolos</span>" + "</td>"
-                }else{
-                    tableBody += "<td>" + "<span class='badge badge-danger'>Tidak Lolos</span>" + "</td>"
-                }
-
-                if (item.time_interview == null) {
-                    tableBody +=
-                        "<td style='padding: 0 10px !important;'  class='text-center '>" +
-                        "<button class='btn btn-sm btn-interview mr-1' data-toggle='modal' data-target='#jadwalInterviewModal' data-id='" +
-                        item.id + "'>Buat jadwal interview</button>"
-                }else if(item.status_interview == 'lolos' ){
-                    tableBody +=
-                        "<td style='padding: 0 10px !important;'  class='text-center '>" +
-                        "<button class='btn btn-sm btn-approve mr-1' data-id='" +
-                        item.id + "'><i class='icon-envelope-open'></i></button>" +
-                        "<button class='btn btn-sm btn-reject mr-1' data-toggle='modal' data-target='#rejectModal' data-id='" +
-                        item.id + "'>Tidak Lolos</button>"
-                }else if(item.status_interview == 'pending' ) {
-                    tableBody +=
-                        "<td style='padding: 0 10px !important;'  class='text-center '>" +
-                        "<button class='btn btn-sm btn-approve mr-1'  data-id='" +
-                        item.id + "'>Lolos</button>" +
-                        "<button class='btn btn-sm btn-reject mr-1' data-toggle='modal' data-target='#rejectModal' data-id='" +
-                        item.id + "'>Tidak Lolos</button>"
-                } else {
-                    tableBody += "<td>" + "-" + "</td>";
-                }
-
-                tableBody += "</tr>";
-                dataNotFound.hide()
-            });
-
-            table.append(tableBody);
-            paginationLink(pagination, responseData);
-            totalData.text(responseData.data.total);
-        } else {
-            table.empty()
-            dataNotFound.show()
-            pagination.empty()
-            totalData.text('0')
+            if (responseData.message === 'Success get data job') {
+                $.each(responseData.data.data, function (index, item) {
+                    let badgeHtml = '';
+    
+                    if (item.status == 'pending') {
+                        badgeHtml = '<span class="badge badge-warning">Sedang di review</span>';
+                    } else if (item.status == 'rejected') {
+                        badgeHtml = '<span class="badge badge-danger">Ditolak</span>';
+                    } else {
+                        badgeHtml = '<span class="badge badge-success">Approve lanjut ke tahap wawancara</span>';
+                    }
+    
+                    tableBody += "<tr>";
+                    tableBody += "<td>" + item.file.pelamar.name + "</td>"
+                    tableBody += "<td>" + item.file.job.name + "</td>"
+    
+                    tableBody += "<td>" + badgeHtml + "</td>"
+                    if (item.time_interview == null) {
+                        tableBody += "<td>" + "-" + "</td>";
+                    } else {
+                        tableBody += "<td>" + item.time_interview + "</td>";
+                    }
+    
+                    if (item.link == null) {
+                        tableBody += "<td>" + "Interview offline" + "</td>";
+                    } else {
+                        tableBody += "<td>" + item.link + "</td>";
+                    }
+    
+                    if (item.status_interview == 'pending') {
+                        tableBody += "<td>" + "<span class='badge badge-warning'>Siap interview</span>" + "</td>"
+                    }else if(item.status_interview == 'lolos') {
+                        tableBody += "<td>" + "<span class='badge badge-success'>Lolos</span>" + "</td>"
+                    }else{
+                        tableBody += "<td>" + "<span class='badge badge-danger'>Tidak Lolos</span>" + "</td>"
+                    }
+    
+                    if (item.time_interview == null) {
+                        tableBody +=
+                            "<td style='padding: 0 10px !important;'  class='text-center '>" +
+                            "<button class='btn btn-sm btn-interview mr-1' data-toggle='modal' data-target='#jadwalInterviewModal' data-id='" +
+                            item.id + "'>Buat jadwal interview</button>"
+                    }else if(item.status_interview == 'lolos' ){
+                        tableBody +=
+                            "<td style='padding: 0 10px !important;'  class='text-center '>" +
+                            "<button class='btn btn-sm btn-approve mr-1' data-id='" +
+                            item.id + "'><i class='icon-envelope-open'></i></button>" +
+                            "<button class='btn btn-sm btn-reject mr-1' data-toggle='modal' data-target='#rejectModal' data-id='" +
+                            item.id + "'>Tidak Lolos</button>"
+                    }else if(item.status_interview == 'pending' ) {
+                        tableBody +=
+                            "<td style='padding: 0 10px !important;'  class='text-center '>" +
+                            "<button class='btn btn-sm btn-approve mr-1'  data-id='" +
+                            item.id + "'>Lolos</button>" +
+                            "<button class='btn btn-sm btn-reject mr-1' data-toggle='modal' data-target='#rejectModal' data-id='" +
+                            item.id + "'>Tidak Lolos</button>"
+                    } else {
+                        tableBody += "<td>" + "-" + "</td>";
+                    }
+    
+                    tableBody += "</tr>";
+                    dataNotFound.hide()
+                });
+    
+                table.append(tableBody);
+                paginationLink(pagination, responseData);
+                totalData.text(responseData.data.total);
+            } else {
+                table.empty()
+                dataNotFound.show()
+                pagination.empty()
+                totalData.text('0')
+            }
+        } catch (error) {
+              table.empty()
+                dataNotFound.show()
+                pagination.empty()
+                totalData.text('0')
         }
+       
     }
 
     async createData(e, id) {
