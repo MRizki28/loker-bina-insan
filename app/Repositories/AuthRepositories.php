4d30e79d1a81e378b74bd19c44c8220ae6e93edf
MRizki28 <muhammadrizkitkj123@gmail.com>
2025-04-18 23:52:39 +0800

    update

--- app/Repositories/AuthRepositories.php
+++ app/Repositories/AuthRepositories.php
@@ -8,6 +8,7 @@ use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
 use MRizki28\ApiResponse\ApiResponse;
 
 class AuthRepositories implements AuthInterfaces
@@ -86,14 +87,146 @@ class AuthRepositories implements AuthInterfaces
 
         $query = $this->userModel->query();
 
-        if($search){
-            $query->where('name', 'like', '%'.$search.'%');
+        if ($search) {
+            $query->where('name', 'like', '%' . $search . '%');
         }
         $data = $query->where('role', 'user')->paginate($limit, ['*'], 'page', $page);
 
-        if($data->isEmpty()){
+        if ($data->isEmpty()) {
             return ApiResponse::notFound();
         }
         return ApiResponse::success($data, 'Success get data user', 200);
+    }
+
+    public function createDataUser(AuthRequest $request)
+    {
+        try {
+            $data = $this->userModel->create([
+                'name' => $request->name,
+                'email' => $request->email,
+                'phone' => $request->phone,
+                'password' => Hash::make('123456'),
+                'role' => $request->role,
+            ]);
+            return ApiResponse::success($data, 'Success create data user', 200);
+        } catch (\Throwable $th) {
+            return ApiResponse::error($th, 500);
+        }
+    }
+
+    public function getAllData(Request $request)
+    {
+        $search = $request->input('search');
+        $limit = $request->input('limit') ? $request->input('limit') : 10;
+        $page = $search ? 1 : (int) $request->input('page', 1);
+
+        $query = $this->userModel->query();
+
+        if ($search) {
+            $query->where('name', 'like', '%' . $search . '%');
+        }
+        $data = $query->paginate($limit, ['*'], 'page', $page);
+
+        if ($data->isEmpty()) {
+            return ApiResponse::notFound();
+        }
+        return ApiResponse::success($data, 'Success get data user', 200);
+    }
+
+    public function getDataById($id)
+    {
+        $data = $this->userModel->find($id);
+        if(!$data){
+            return ApiResponse::notFound();
+        }
+
+        return ApiResponse::success($data, 'SUccess get data by id', 200);
+    }
+
+    public function updateDataUser(AuthRequest $request, $id)
+    {
+        try {
+            $data = $this->userModel->find($id);
+            if (!$data) {
+                return ApiResponse::notFound();
+            }
+
+            $data->name = $request->input('name');
+            $data->email = $request->input('email');
+            $data->phone = $request->input('phone');
+            $data->password = Hash::make('123456');
+            $data->role = $request->input('role');
+            $data->save();
+
+            return ApiResponse::success($data, 'Success update data user', 200);
+        } catch (\Throwable $th) {
+            return ApiResponse::error($th, 500);
+        }
+    }
+
+    public function deleteData($id)
+    {
+        try {
+            $id_login = Auth::user()->id;
+            $data = $this->userModel->find($id);
+
+            if($id === $id_login){
+                return response()->json([
+                    'status' => 'error',
+                    'message' => 'Cannot delete your self'
+                ],422);
+            }
+
+            $data->delete();
+
+            return response()->json([
+                'status' => 'success',
+                'message' => 'Success delete user'
+            ]);
+        } catch (\Throwable $th) {
+            return ApiResponse::error($th, 500);
+        }
+    }
+
+    public function setting(Request $request)
+    {
+        try {
+            $validation = Validator::make($request->all(), [
+                'password_old' => 'required',
+                'password' => 'required|confirmed|min:6',
+                'password_confirmation' => 'required'
+            ]);
+
+            if ($validation->fails()) {
+                return response()->json([
+                    'status' => 'not validate',
+                    'message' => 'Check your validation',
+                    'errors' => $validation->errors()
+                ],422);
+            }
+
+            $id = Auth::user()->id;
+            $data = $this->userModel->where('id', $id)->first();
+            if (!Hash::check($request->password_old, $data->password)) {
+                return response()->json([
+                    'code' => 200,
+                    'message' => 'Old password is wrong'
+                ]);
+            }
+
+            $data->password = Hash::make($request->input('password'));
+            $data->save();
+            if ($data) {
+                return ApiResponse::success($data, 'Success update password', 200);
+                Auth::guard('web')->logout();
+            } else {
+                return response()->json([
+                    'status' => 'error',
+                    'message' => 'Failed update password'
+                ], 422);
+            }
+        } catch (\Throwable $th) {
+            return ApiResponse::error($th, 500);
+        }
     }
 }
