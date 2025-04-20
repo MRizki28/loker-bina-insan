f696407088d5234d4c14a8fcc872d1c57813a4f1
MRizki28 <muhammadrizkitkj123@gmail.com>
2025-04-20 19:42:24 +0800

    update

---
+++ resources/views/pages/settingAhp.blade.php
@@ -0,0 +1,59 @@
+@extends('Layouts.Base')
+@section('content')
+    <div class="page-inner">
+        <x-base-header headerName="Data bobot kriteria" headerIcon="fas fa-file-image"></x-base-header>
+        <x-base-body>
+            <x-base-sorting addSearching="true" addNotificationAll="false" addUpdateStatus="false"
+                modalUpdateStatus="#updateStatusModal" buttonAdd="true" headerAddButton="Tambah Data"
+                modalId="#bobotKriteriaModal"></x-base-sorting>
+            <x-base-table initId="table">
+                <x-slot name="thead">
+                    <tr class="text-center">
+                        <th>Nama kriteria</th>
+                        <th>Bobot prioriti kriteria</th>
+                        <th>Aksi</th>
+                    </tr>
+                </x-slot>
+            </x-base-table>
+        </x-base-body>
+        <x-base-header headerName="Data bobot alternatif" headerIcon="fas fa-file-image"></x-base-header>
+        <x-base-body>
+            <x-base-sorting addSearching="true" addNotificationAll="false" addUpdateStatus="false"
+                modalUpdateStatus="#updateStatusModal" buttonAdd="true" headerAddButton="Tambah Data"
+                modalId="#lokerModal"></x-base-sorting>
+            <div class="table-responsive">
+                <table class="table" id="table-alternatif">
+                    <thead style="background-color: #f7f8fa;">
+                        <tr class="text-center">
+                            <th>Nama alternatif</th>
+                            <th>Sub kriteria</th>
+                            <th>Bobot prioriti alternatif</th>
+                            <th>Aksi</th>
+                        </tr>
+                    </thead>
+                    <tbody class="text-center">
+
+                    </tbody>
+                    <tfoot id="dataNotFound">
+                        <tr class="text-center text-muted" id="template-empty-info">
+                            <td colspan="9" class=" ">
+                                <i class="fas fa-folder-open mr-1"></i> Data tidak ditemukan ...
+                            </td>
+                        </tr>
+                    </tfoot>
+                </table>
+                <div class="d-flex justify-content-between align-items-center px-4">
+                    <span class="mb-3 text-muted">
+                        Total <span id="data-total"></span> data
+                    </span>
+                    <ul class="pagination-alternatif pg-info"></ul>
+                </div>
+            </div>
+        </x-base-body>
+    </div>
+    <x-bobotkriteria.bobot-kriteria-modal></x-bobotkriteria.bobot-kriteria-modal>
+@endsection
+
+@section('js-service')
+    <script type="module" src="{{ asset('js/ahp.controller.js') }}?v={{ time() }}"></script>
+@endsection
