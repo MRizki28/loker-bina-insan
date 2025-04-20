0f03e465b8a114de35bf5c6fd0600ceb0b28e6cd
MRizki28 <muhammadrizkitkj123@gmail.com>
2025-04-20 19:41:42 +0800

    update

---
+++ app/Models/BobotKriteriaModel.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class BobotKriteriaModel extends Model
+{
+    use HasFactory, HasUuids;
+
+    protected $table = 'tb_bobot_kriteria';
+    protected $fillable = [
+        'id',
+        'name_kriteria',
+        'bobot_prioriti_kriteria',
+    ];
+}
