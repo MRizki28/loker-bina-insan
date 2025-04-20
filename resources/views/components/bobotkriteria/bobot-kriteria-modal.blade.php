312f8b9dec10ba4fd99d2e0a1155b6b2e01a8d79
MRizki28 <muhammadrizkitkj123@gmail.com>
2025-04-20 19:42:15 +0800

    update

---
+++ resources/views/components/bobotkriteria/bobot-kriteria-modal.blade.php
@@ -0,0 +1,37 @@
+<div class="modal fade" id="bobotKriteriaModal" tabindex="-1" role="dialog" aria-labelledby="jobLabel" aria-hidden="true">
+    <div class="modal-dialog" role="document" style="max-width: 800px; ">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="modal-title">Tambah Data</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <form id="formTambah">
+                <div class="modal-body">
+                    @csrf
+                    <div class="row py-2">
+                        <div class="col-md-6">
+                            <input type="hidden" name="id" id="id" value="">
+                            <div class="form-group fill form-show-validation">
+                                <label>Nama kriteria</label>
+                                <input type="text" name="name_kriteria" id="name_kriteria" class="form-control"
+                                    placeholder="Nama kriteria">
+                            </div>
+                        </div>
+                        <div class="col-md-6">
+                            <div class="form-group fill form-show-validation">
+                                <label>Bobot prioriti kriteria</label>
+                                <input type="number" name="bobot_prioriti_kriteria" id="bobot_prioriti_kriteria"
+                                    class="form-control" placeholder="Bobot prioriti kriteria">
+                            </div>
+                        </div>
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-outline-danger" data-dismiss="modal">Tutup</button>
+                        <button type="submit" class="btn btn-outline-primary">Simpan Data</button>
+                    </div>
+            </form>
+        </div>
+    </div>
+</div>
