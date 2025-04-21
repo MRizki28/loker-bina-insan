24814eaecf4fb7e53e7a0fceaa3771152e1044a0
MRizki28 <muhammadrizkitkj123@gmail.com>
2025-04-21 21:52:45 +0800

    update

--- resources/views/pages/settingAhp.blade.php
+++ resources/views/pages/settingAhp.blade.php
@@ -4,7 +4,7 @@
         <x-base-header headerName="Data bobot kriteria" headerIcon="fas fa-file-image"></x-base-header>
         <x-base-body>
             <x-base-sorting addSearching="true" addNotificationAll="false" addUpdateStatus="false"
-                modalUpdateStatus="#updateStatusModal" buttonAdd="true" headerAddButton="Tambah Data"
+                modalUpdateStatus="#updateStatusModal" buttonAdd="false" headerAddButton="Tambah Data"
                 modalId="#bobotKriteriaModal"></x-base-sorting>
             <x-base-table initId="table">
                 <x-slot name="thead">
@@ -18,9 +18,20 @@
         </x-base-body>
         <x-base-header headerName="Data bobot alternatif" headerIcon="fas fa-file-image"></x-base-header>
         <x-base-body>
-            <x-base-sorting addSearching="true" addNotificationAll="false" addUpdateStatus="false"
-                modalUpdateStatus="#updateStatusModal" buttonAdd="true" headerAddButton="Tambah Data"
-                modalId="#lokerModal"></x-base-sorting>
+            <div class="p-3">
+                <div class="d-flex justify-content-between align-items-center mb-md-0 row">
+                    <div class="col-md-7 row mb-2 ">
+                        {{-- Form Search --}}
+                        <div class="input-icon col-md-4">
+                            <input type="text" class="form-control" placeholder="Cari..." id="form-search-alternatif">
+                            <span class="input-icon-addon p-3 text-center" id="search-button-alternatif">
+                                <i class="fa fa-search " style="cursor: pointer;"></i>
+                            </span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
             <div class="table-responsive">
                 <table class="table" id="table-alternatif">
                     <thead style="background-color: #f7f8fa;">
@@ -34,7 +45,7 @@
                     <tbody class="text-center">
 
                     </tbody>
-                    <tfoot id="dataNotFound">
+                    <tfoot id="dataNotFoundAlternatif">
                         <tr class="text-center text-muted" id="template-empty-info">
                             <td colspan="9" class=" ">
                                 <i class="fas fa-folder-open mr-1"></i> Data tidak ditemukan ...
@@ -44,12 +55,13 @@
                 </table>
                 <div class="d-flex justify-content-between align-items-center px-4">
                     <span class="mb-3 text-muted">
-                        Total <span id="data-total"></span> data
+                        Total <span id="data-total-alternatif"></span> data
                     </span>
-                    <ul class="pagination-alternatif pg-info"></ul>
+                    {{-- <ul class="pagination pg-info"></ul> --}}
                 </div>
             </div>
         </x-base-body>
+        <x-bobotalternatif.bobot-alternatif-modal></x-bobotalternatif.bobot-alternatif-modal>
     </div>
     <x-bobotkriteria.bobot-kriteria-modal></x-bobotkriteria.bobot-kriteria-modal>
 @endsection
