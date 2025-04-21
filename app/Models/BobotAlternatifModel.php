8fcc9e596878245b3cc76ee03dc416315447b292
MRizki28 <muhammadrizkitkj123@gmail.com>
2025-04-21 21:50:47 +0800

    update

--- app/Models/BobotAlternatifModel.php
+++ app/Models/BobotAlternatifModel.php
@@ -2,15 +2,22 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class BobotAlternatifModel extends Model
 {
+    use HasFactory, HasUuids;
     protected $table = 'tb_bobot_alternatif';
     protected $fillable = [
         'id',
-        'id_kreteria',
+        'id_kriteria',
         'name_alternatif',
         'bobot_prioriti_alternatif',
     ];
+
+    public function kriteria(){
+        return $this->belongsTo(BobotKriteriaModel::class, 'id_kriteria', 'id');
+    }
 }
