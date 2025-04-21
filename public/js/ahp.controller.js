832c292b47077c5610300bd3c76d3f33d424a667
MRizki28 <muhammadrizkitkj123@gmail.com>
2025-04-21 21:52:23 +0800

    update

--- public/js/ahp.controller.js
+++ public/js/ahp.controller.js
@@ -3,6 +3,7 @@ import AhpService from "./ahp.service.js?v=1.0.0";
 $(document).ready(function () {
     const ahpService = new AhpService();
     ahpService.getDataKriteria()
+    ahpService.getDataAlternatif()
 
     $(document).on('click', '.page-link', function (e) {
         e.preventDefault();
@@ -12,31 +13,24 @@ $(document).ready(function () {
         ahpService.getDataKriteria(fullUrl);
     });
 
-    $(document).on('keyup', function (e) {
-        if (e.keyCode === 13) {
-            ahpService.getDataKriteria();
-        }
-    })
 
     $(document).on('click', '#search-button', function () {
         ahpService.getDataKriteria();
     })
 
+    $(document).on('click', '#search-button-alternatif', function () {
+        ahpService.getDataAlternatif();
+    })
+
     function validation() {
         $('#formTambah').validate({
             rules: {
-                "name_kriteria": {
-                    required: true,
-                },
                 "bobot_prioriti_kriteria": {
                     required: true,
                 }
 
             },
             messages: {
-                "name_kriteria": {
-                    required: 'Nama kriteria wajib diisi',
-                },
                 "bobot_prioriti_kriteria": {
                     required: 'Bobot prioriti kriteria wajib diisi',
                 }
@@ -54,6 +48,33 @@ $(document).ready(function () {
     }
 
     validation();
+    function validation2() {
+        $('#formTambahAlternatif').validate({
+            rules: {
+                "bobot_prioriti_alternatif": {
+                    required: true,
+                }
+
+            },
+            messages: {
+                "bobot_prioriti_alternatif": {
+                    required: 'Bobot prioriti alternatif wajib diisi',
+                }
+            },
+            highlight: function (element) {
+                $(element).closest('.form-group').removeClass('has-success').addClass('has-error');
+            },
+            success: function (element) {
+                $(element).closest('.form-group').removeClass('has-error').addClass('has-success');
+            },
+            errorPlacement: function (error, element) {
+                error.insertAfter(element);
+            }
+        });
+    }
+
+
+    validation2();
 
     function checkingEdit() {
         return $('#id').val() ? true : false
@@ -74,11 +95,19 @@ $(document).ready(function () {
         ahpService.deleteDataKriteria(id)
     })
 
+    $('#formTambahAlternatif').on('submit', function (e) {
+        e.preventDefault();
+        ahpService.updateDataAlternatif(e, resetFieldAlternatif);
+    });
+
+    $(document).on('click', '.edit-modal-alternatif', function () {
+        const id = $(this).data('id')
+        ahpService.getDataByIdAlternatif(id)
+    })
 
 
     function resetField() {
         $('#bobotKriteriaModal').on('hidden.bs.modal', function () {
-            $('#name_kriteria').val('');
             $('#bobot_prioriti_kriteria').val();
             $('.form-control').removeClass('has-error').removeClass('has-success');
             $('.form-group').removeClass('has-error').removeClass('has-success');
@@ -86,6 +115,18 @@ $(document).ready(function () {
             $('.has-error').remove();
         });
     }
+
+    function resetFieldAlternatif() {
+        $('#bobotAlternatif').on('hidden.bs.modal', function () {
+            $('#bobot_prioriti_alternatif').val();
+            $('.form-control').removeClass('has-error').removeClass('has-success');
+            $('.form-group').removeClass('has-error').removeClass('has-success');
+            $('#formTambahAlternatif').find('.form-group').find('.error').remove()
+            $('.has-error').remove();
+        });
+    }
+
+    resetFieldAlternatif()
 
     resetField()
 });
