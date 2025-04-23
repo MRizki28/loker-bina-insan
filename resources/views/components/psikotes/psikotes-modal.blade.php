684b05177f332c062913bdf8981fb8469f6bd866
Rizki <muhammadrizkitkj123@gmail.com>
2025-04-23 12:07:45 +0800

    update

---
+++ resources/views/components/psikotes/psikotes-modal.blade.php
@@ -0,0 +1,30 @@
+<div class="modal fade" id="jadwalPsikotesModal" tabindex="-1" role="dialog" aria-labelledby="jobLabel" aria-hidden="true">
+    <div class="modal-dialog" role="document" style="max-width: 800px; ">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="modal-title">Tambah Data</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <form id="formTambah">
+                <div class="modal-body">
+                    @csrf
+                    <div class="row py-2">
+                        <div class="col-md-12">
+                            <input type="hidden" name="id" id="id" value="">
+                            <div class="form-group fill form-show-validation">
+                                <label>Waktu test</label>
+                                <input type="datetime-local" id="time_psikotes" name="time_psikotes" class="form-control"
+                                    placeholder="Tanggal dan waktu">
+                            </div>
+                        </div>
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-outline-danger" data-dismiss="modal">Tutup</button>
+                        <button type="submit" class="btn btn-outline-primary">Simpan Data</button>
+                    </div>
+            </form>
+        </div>
+    </div>
+</div>
