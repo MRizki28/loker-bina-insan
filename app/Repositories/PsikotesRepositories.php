3ce02633e91a6b7b506ddc14722e10b8ceac5722
Rizki <muhammadrizkitkj123@gmail.com>
2025-04-23 12:06:55 +0800

    update

--- app/Repositories/PsikotesRepositories.php
+++ app/Repositories/PsikotesRepositories.php
@@ -5,21 +5,28 @@ namespace App\Repositories;
 
 use App\Http\Requests\Ngaji\NgajiRequest;
 use App\Http\Requests\Psikotes\PsikotesRequest;
-use App\Interfaces\NgajiInterfaces;
 use App\Interfaces\PsikotesInterfaces;
 use App\Jobs\EmailHandlerJob;
+use App\Models\ArchiveModel;
 use App\Models\NgajiModel;
 use App\Models\PsikotesModel;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Validator;
 use MRizki28\ApiResponse\ApiResponse;
 
 class PsikotesRepositories implements PsikotesInterfaces
 {
     protected $psikotesModal;
-    public function __construct(PsikotesModel $psikotesModal)
+    protected $archiveModel;
+    protected $ngajiModel;
+    protected $ngajiInterfaces;
+
+    public function __construct(PsikotesModel $psikotesModal, ArchiveModel $archiveModel, NgajiModel $ngajiModel)
     {
         $this->psikotesModal = $psikotesModal;
+        $this->archiveModel = $archiveModel;
+        $this->ngajiModel = $ngajiModel;
     }
 
     public function getAllData(Request $request){
@@ -30,7 +37,7 @@ class PsikotesRepositories implements PsikotesInterfaces
             $query = $this->psikotesModal->query();
 
             if ($search) {
-                $query->whereHas('pelamar', function ($q) use ($search) {
+                $query->whereHas('interview.file.pelamar', function ($q) use ($search) {
                     $q->where('name', 'like', '%' . $search . '%');
                 });
             }
@@ -43,7 +50,7 @@ class PsikotesRepositories implements PsikotesInterfaces
                 return ApiResponse::notFound();
             }
 
-            return ApiResponse::success($data, 'Success get ngaji', 200);
+            return ApiResponse::success($data, 'Success get psikotes', 200);
         } catch (\Throwable $th) {
             return ApiResponse::error($th, 500);
         }
@@ -56,23 +63,75 @@ class PsikotesRepositories implements PsikotesInterfaces
 
     public function approve(Request $request, $id)
     {
+        DB::beginTransaction();
         try {
-            $ngaji = $this->psikotesModal->find($id);
-            if (!$ngaji) {
-                return ApiResponse::notFound('Ngaji not found');
+            $data = $this->psikotesModal->find($id);
+            if (!$data) {
+                return ApiResponse::notFound();
             }
 
-            $ngaji->status_ngaji = 'approved';
-            $ngaji->save();
+            $data->status_psikotes = 'lolos';
+            $data->save();
 
-            return ApiResponse::success($ngaji, 'Ngaji approved successfully', 200);
+            DB::commit();
+            $archive = $this->archiveModel->where('id_file', $data->interview->id_berkas)->first();
+            if ($archive) {
+                $archive->status_psikotes = 'lolos';
+                $archive->save();
+            }
+
+            EmailHandlerJob::dispatch('Selamat anda lolos psikotes, dan silahkan check pada sistem untuk jadwal test mengaji', $data->interview->file->pelamar->email);
+
+            if ($archive->status_psikotes == 'lolos') {
+                $this->ngajiModel->create([
+                    'id_psikotes' => $data->id,
+                ]);
+            }
+            return ApiResponse::success($data, 'Success update data job', 200);
         } catch (\Throwable $th) {
             return ApiResponse::error($th, 500);
         }
     }
     public function reject(Request $request, $id)
     {
-        
+        try {
+            $validation = Validator::make($request->all(), [
+                'reason_reject' => 'required',
+            ]);
+            if ($validation->fails()) {
+                return response()->json([
+                    'status' => 'not validate',
+                    'message' => $validation->errors(),
+                ], 422);
+            }
+
+            $data = $this->psikotesModal->find($id);
+            if (!$data) {
+                return ApiResponse::notFound();
+            }
+
+            $data->status_psikotes = 'gagal';
+            $data->reason_reject_psikotes = $request->input('reason_reject');
+            $data->save();
+
+            DB::commit();
+            $archive = $this->archiveModel->where('id_file', $data->interview->id_berkas)->first();
+            if ($archive) {
+                $archive->status_psikotes = 'gagal';
+                $archive->save();
+            }
+
+            EmailHandlerJob::dispatch('Maaf anda tidak lolos seleksi wawancara ' . $data->reason_reject_psikotes, $data->interview->file->pelamar->email);
+            if ($archive->status_psikotes == 'gagal') {
+                $psikotes = $this->psikotesModal->where('id', $data->id)->first();
+                if ($psikotes) {
+                    $psikotes->delete();
+                }
+            }
+            return ApiResponse::success($data, 'Success update data job', 200);
+        } catch (\Throwable $th) {
+            return ApiResponse::error($th, 500);
+        }
     }
 
     public function updateData(PsikotesRequest $request, $id)
@@ -84,19 +143,19 @@ class PsikotesRepositories implements PsikotesInterfaces
             if (!$data) {
                 return ApiResponse::notFound();
             }
-            $data->time_test = $request->input('time_test');
+            $data->time_psikotes = $request->input('time_psikotes');
             $data->save();
             DB::commit();
 
-            // $archive = $this->archiveModel->where('id_file', $data->id_berkas)->first();
-            // if ($archive) {
-            //     $archive->time_interview = $data->time_interview;
-            //     $archive->link = $data->link;
-            //     $archive->save();
-            // }
+            $archive = $this->archiveModel->where('id_file', $data->id_berkas)->first();
+            if ($archive) {
+                $archive->time_interview = $data->time_interview;
+                $archive->link = $data->link;
+                $archive->save();
+            }
 
-            EmailHandlerJob::dispatch('Berikut jadwal psikotes anda ' . $data->time_test . ' untuk lokasinya SIT BINA INSAN PALU Terimakasih', $data->interview->file->pelamar->email);
-            return ApiResponse::success($data, 'Success update waktu test ngaji', 200);
+            EmailHandlerJob::dispatch('Berikut jadwal psikotes anda ' . $data->time_psikotes . ' untuk lokasinya SIT BINA INSAN PALU Terimakasih', $data->interview->file->pelamar->email);
+            return ApiResponse::success($data, 'Success update waktu psikotes', 200);
         } catch (\Throwable $th) {
             DB::rollBack();
             return ApiResponse::error($th, 500);
