4dafbb2c35eb4c53dee869740b77b1409f5b93d4
Rizki <muhammadrizkitkj123@gmail.com>
2025-04-23 17:27:14 +0800

    update

--- app/Repositories/PenilaianRepositories.php
+++ app/Repositories/PenilaianRepositories.php
@@ -221,10 +221,55 @@ class PenilaianRepositories implements PenilaianInterfaces
             return ApiResponse::notFound();
         }
 
-        return ApiResponse::success($data, 'Success get kriteria for interview review', 200);
+        return ApiResponse::success($data, 'Success get kriteria for ngaji review', 200);
     }
 
     public function createPenilaianNgaji(Request $request) {
+        DB::beginTransaction();
+        try {
+            $validation = Validator::make($request->all(), [
+                'id_file' => 'required|uuid',
+                'id_bobot_kriteria' => 'required|array',
+                'id_bobot_kriteria.*' => 'required|uuid',
+                'id_bobot_alternatif' => 'required|array',
+                'id_bobot_alternatif.*' => 'required|uuid',
+            ]);
 
+            if ($validation->fails()) {
+                return response()->json([
+                    'status' => 'not validate',
+                    'message' => 'check your validation',
+                    'errors' => $validation->errors()
+                ], 422);
+            }
+
+            $dataPenilaian = $this->penilaianModel::where('id_file', $request->input('id_file'))->first();
+
+            if (!$dataPenilaian) {
+                $dataPenilaian = new $this->penilaianModel();
+                $dataPenilaian->id_file = $request->input('id_file');
+                $dataPenilaian->save();
+            }
+
+            foreach ($request->input('id_bobot_kriteria') as $key => $value) {
+                $bobotPrioritiKriteria = $this->bobotKriteriaModel::find($value);
+                $bobotPrioritiAlternatif = $this->bobotAlternatifModel::find($request->input('id_bobot_alternatif')[$key]);
+
+                $dataDetailPenilaian = new $this->detailPenilaianModel();
+                $dataDetailPenilaian->id_penilaian = $dataPenilaian->id;
+                $dataDetailPenilaian->id_bobot_kriteria = $value;
+                $dataDetailPenilaian->id_bobot_alternatif = $request->input('id_bobot_alternatif')[$key];
+                $dataDetailPenilaian->bobot_prioriti_kriteria = $bobotPrioritiKriteria->bobot_prioriti_kriteria;
+                $dataDetailPenilaian->bobot_prioriti_alternatif = $bobotPrioritiAlternatif->bobot_prioriti_alternatif;
+                $dataDetailPenilaian->save();
+            }
+
+            DB::commit();
+
+            return ApiResponse::success($dataDetailPenilaian, 'Success create penilaian psikotes', 200);
+        } catch (\Throwable $th) {
+            DB::rollBack();
+            return ApiResponse::error($th, 500);
+        }
     }
 }
