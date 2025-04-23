6ca1e6762e7f9fbbaebbbf56dd22330e8d8ebcd0
MRizki28 <muhammadrizkitkj123@gmail.com>
2025-04-23 08:59:48 +0800

    update

--- app/Interfaces/PenilaianInterfaces.php
+++ app/Interfaces/PenilaianInterfaces.php
@@ -7,4 +7,8 @@ use Illuminate\Http\Request;
 interface PenilaianInterfaces {
     public function createPenilaianBerkas(Request $request);
     public function getKriteriaForBerkasReview();
+    public function getKriteriaForInterviewReview();
+    public function createPenilaianInterview(Request $request);
+    public function getKriteriaForNgaji();
+    public function createPenilaianNgaji(Request $request);
 }
