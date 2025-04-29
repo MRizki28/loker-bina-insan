e801160524a0d347dcbf86845bba37dcb3f78c12
Rizki <muhammadrizkitkj123@gmail.com>
2025-04-29 11:37:15 +0800

    update

--- app/Repositories/InterviewRepositories.php
+++ app/Repositories/InterviewRepositories.php
@@ -10,7 +10,6 @@ use App\Models\InterviewModel;
 use App\Models\PsikotesModel;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
-use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Validator;
 use MRizki28\ApiResponse\ApiResponse;
 
