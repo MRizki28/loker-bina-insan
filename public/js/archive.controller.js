04a3cae47a783e1ed7963274ab1e866819fa0d1a
MRizki28 <muhammadrizkitkj123@gmail.com>
2025-05-02 11:13:56 +0800

    update

--- public/js/archive.controller.js
+++ public/js/archive.controller.js
@@ -1,4 +1,4 @@
-import ArchiveService from "./archive.service.js?v=1.0.0";
+import ArchiveService from "./archive.service.js?v=1.0.1";
 
 $(document).ready(function () {
     const archiveService = new ArchiveService()
