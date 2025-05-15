9def31b35b3748ef6204441dc4a1a93777ce7262
MRizki28 <muhammadrizkitkj123@gmail.com>
2025-05-15 11:12:38 +0800

    update

--- public/js/fileapply.controller.js
+++ public/js/fileapply.controller.js
@@ -1,4 +1,4 @@
-import FileApplyService from "./fileapply.service.js?v=1.0.1";
+import FileApplyService from "./fileapply.service.js?v=1.0.2";
 
 $(document).ready(function () {
     const fileApplyService = new FileApplyService()
